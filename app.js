eb5b2facc13caeb290fd51b983dace8239506755
Gareth George <garethgeorge97@gmail.com>
2015-02-04 16:35:49 -0800

    added button for creating items

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-angular.module('train-app', [])
-.controller('TrainSchedule', ['$scope', function($scope) {
-  $scope.trainTimes = [
-    {name: 'Palo Alto', time: '15 minutes'},
-    {name: 'San Francisco', time: '20 minutes'},
-    {name: 'Mountain View', time: '30 minutes'}
+angular.module('events-app', [])
+.controller('EventSchedule', ['$scope', function($scope) {
+  $scope.events = [
+    {name: 'Learn AngularJS', time: '4:34'}
   ];
-}]);
+  $scope.eventName = '';
+  $scope.eventTime = '';
+  $scope.addEvent = function() {
+    $scope.events.push({
+      name: $scope.eventName,
+      time: $scope.eventTime
+    });
+    $scope.eventName = '';
+    $scope.eventTime = '';
+  }
+}])
